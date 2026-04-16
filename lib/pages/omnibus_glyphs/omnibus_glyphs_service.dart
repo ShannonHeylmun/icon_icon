a1a96857e0de6415baaf7fa42b287b9196d8f412
Shannon Heylmun <sajshannon@gmail.com>
2026-04-16 18:47:17 -0400

    Update omnibus search service with singleton

--- lib/pages/omnibus_glyphs/omnibus_glyphs_service.dart
+++ lib/pages/omnibus_glyphs/omnibus_glyphs_service.dart
@@ -1,7 +1,45 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:rxdart/subjects.dart';
 
 class OmnibusGlyphsService {
-  final searchController = TextEditingController();
+  // singleton pattern
+  static final OmnibusGlyphsService _instance =
+      OmnibusGlyphsService._internal();
+  factory OmnibusGlyphsService() {
+    return _instance;
+  }
+  OmnibusGlyphsService._internal() {
+    _refinedListBehaviorSubject.add([]);
+    _searchController.addListener(_updateRefinedList);
+  }
 
-  // Add any additional logic or methods related to Omnibus Glyphs here
+  void dispose() {
+    _searchController.removeListener(_updateRefinedList);
+  }
+
+  void _updateRefinedList() {
+    String searchTerm = _searchController.text.toLowerCase();
+    List<(String, Object)> refined = _allIcons.where((icon) {
+      return _matchesSearch(icon, searchTerm);
+    }).toList();
+    _refinedListBehaviorSubject.add(refined);
+  }
+
+  bool _matchesSearch((String, Object) icon, String searchTerm) {
+    return icon.$1.toLowerCase().contains(searchTerm);
+  }
+
+  final List<(String, Object)> _allIcons = [];
+
+  final TextEditingController _searchController = TextEditingController();
+  TextEditingController get searchController => _searchController;
+  late List<(String, Object)> _refinedList;
+  List<(String, Object)> get refinedList => _refinedList;
+
+  final BehaviorSubject<List<(String, Object)>> _refinedListBehaviorSubject =
+      BehaviorSubject.seeded([]);
+  Stream<List<(String, Object)>> get refinedListStream =>
+      _refinedListBehaviorSubject.stream;
 }
