d53495b1e905b2c03661baa6dda67c759a92c094
Shannon Heylmun <sajshannon@gmail.com>
2026-04-16 20:16:17 -0400

    Update the omnibus search screen to display the icons

--- lib/pages/omnibus_glyphs/omnibus_glyphs_screen.dart
+++ lib/pages/omnibus_glyphs/omnibus_glyphs_screen.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:icon_icon/colors.dart';
 import 'package:icon_icon/components/bottom_search_card.dart';
 import 'package:icon_icon/components/custom_app_bar.dart';
+import 'package:icon_icon/components/responsive_icons.dart';
 import 'package:icon_icon/pages/omnibus_glyphs/omnibus_glyphs_service.dart';
 
 class OmnibusGlyphsScreen extends StatelessWidget {
@@ -20,19 +21,19 @@ class OmnibusGlyphsScreen extends StatelessWidget {
             Positioned(
               top: 8,
               child: Text(
-                "❄️",
-                style: GoogleFonts.notoEmoji(
-                  color: omnibusGlyphsColorContrast,
-                  fontSize: 30,
-                ),
-              ),
-            ),
-          ],
+          "❄️",
+          style: GoogleFonts.notoEmoji(
+            color: omnibusGlyphsColorContrast,
+            fontSize: 30,
+          ),
         ),
         titleText: "Search All",
       ),
-      body: const Center(
-        child: Text('Omnibus Glyphs Search Screen Coming Soon!'),
+      body: StreamBuilder(
+        builder: (context, snapshot) => snapshot.hasData
+            ? ResponsiveIcons(iconsToShow: snapshot.data!)
+            : Center(child: Text("Search All Glyphs")),
+        stream: OmnibusGlyphsService().refinedListStream,
       ),
       bottomNavigationBar: BottomSearchCard(
         controller: OmnibusGlyphsService().searchController,
